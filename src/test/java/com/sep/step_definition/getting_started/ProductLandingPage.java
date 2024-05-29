fd543da192246aa69d4bc96d87a2c9e89ec853f7
Olesya <oltekuteva@gmail.com>
2024-05-29 10:28:30 -0500

    extra blank lines were removed from ProductLandingPage step def Validation for program name was changed from isDisplayed() to equals() method in order to use examples from feature file

--- src/test/java/com/sep/step_definition/getting_started/ProductLandingPage.java
+++ src/test/java/com/sep/step_definition/getting_started/ProductLandingPage.java
@@ -19,39 +19,25 @@ public class ProductLandingPage {
         String actualSecureText=leftMainPage.getSecureCheckout.getText();
         System.out.println(actualSecureText);
         Assertions.assertEquals(secureCheckout, actualSecureText);
-
-
     }
-
-
     @Then("the system should display the {string}")
     public void the_system_should_display_the(String programName) {
-        Assertions.assertTrue(leftMainPage.programName.isDisplayed());
-
+        Assertions.assertEquals(programName,leftMainPage.programName.getText());
     }
    @Then("the footer on the left side should display logo")
     public void the_footer_on_the_left_side_should_display_logo() {
         Assertions.assertTrue(leftMainPage.footerCydeoImage.isDisplayed());
-
     }
-
     @Then("the footer on the left side should display in following order")
     public void the_footer_on_the_left_side_should_display_in_following_order(List <String> leftFooterElements) {
         List <WebElement> footerElements=leftMainPage.footerElements;
         for (int i = 0; i < footerElements.size(); i++) {
             Assertions.assertEquals(leftFooterElements.get(i),footerElements.get(i).getText());
-
-
         }
-
 }
     @Then("the footer on the right side should display {string}")
     public void the_footer_on_the_right_side_should_display(String rightFooterText) {
         String actualFooterText=startApplicationPage.footer.getText();
         Assertions.assertEquals(actualFooterText, rightFooterText);
-
     }
-
-
-
 }
